9fe55a02346029a6fbf5cea9720fcb72c82e8ace
Proabhi123 <101856743+Proabhi123@users.noreply.github.com>
2023-09-20 12:05:46 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version of js.
+
+// alert(3 + 3) // we are using node js, not browser 
+
+let name = "Abhijeet"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean = true/false
+// null => standalone value / or null is a type 
+// undefined => value is not defined
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
